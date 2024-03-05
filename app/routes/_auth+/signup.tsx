a9fe98b63e69d8d1fda9641c9c31feea4222db95
Fabian Juren <fjuren@gmail.com>
2024-03-04 21:27:18 -0800

    Add meta to signup page

--- app/routes/_auth+/signup.tsx
+++ app/routes/_auth+/signup.tsx
@@ -1,3 +1,15 @@
+import { MetaFunction } from '@remix-run/node';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Signup' },
+    {
+      name: 'description',
+      content: 'Sign up for an account',
+    },
+  ];
+};
+
 export default function SignupRoute() {
   return (
     <div>
