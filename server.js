5a2d98e4234e4468034a075922bd140d5a61d01c
Fabian Juren <fjuren@gmail.com>
2024-04-11 07:59:45 -0700

    Add some rate limiting to pages, aggressive limiting on signup and login pages for now, less aggress of general get requests

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import * as url from "node:url";
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
 
-import { createRequestHandler } from "@remix-run/express";
-import { broadcastDevReady, installGlobals } from "@remix-run/node";
-import compression from "compression";
-import express from "express";
-import morgan from "morgan";
-import sourceMapSupport from "source-map-support";
+import { createRequestHandler } from '@remix-run/express';
+import { broadcastDevReady, installGlobals } from '@remix-run/node';
+import compression from 'compression';
+import express from 'express';
+import morgan from 'morgan';
+import sourceMapSupport from 'source-map-support';
+import { rateLimit } from 'express-rate-limit';
 
 sourceMapSupport.install({
   retrieveSourceMap: function (source) {
-    const match = source.startsWith("file://");
+    const match = source.startsWith('file://');
     if (match) {
       const filePath = url.fileURLToPath(source);
       const sourceMapPath = `${filePath}.map`;
       if (fs.existsSync(sourceMapPath)) {
         return {
           url: source,
-          map: fs.readFileSync(sourceMapPath, "utf8"),
+          map: fs.readFileSync(sourceMapPath, 'utf8'),
         };
       }
     }
@@ -29,12 +30,12 @@ installGlobals();
 
 /** @typedef {import('@remix-run/node').ServerBuild} ServerBuild */
 
-const BUILD_PATH = path.resolve("build/index.js");
-const VERSION_PATH = path.resolve("build/version.txt");
+const BUILD_PATH = path.resolve('build/index.js');
+const VERSION_PATH = path.resolve('build/version.txt');
 
 const initialBuild = await reimportServer();
 const remixHandler =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? await createDevRequestHandler(initialBuild)
     : createRequestHandler({
         build: initialBuild,
@@ -46,27 +47,60 @@ const app = express();
 app.use(compression());
 
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // Remix fingerprints its assets so we can cache forever.
 app.use(
-  "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
+  '/build',
+  express.static('public/build', { immutable: true, maxAge: '1y' })
 );
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
-app.use(express.static("public", { maxAge: "1h" }));
+app.use(express.static('public', { maxAge: '1h' }));
 
-app.use(morgan("tiny"));
+const maxMultiple = process.env.TESTING ? 10_000 : 1;
 
-app.all("*", remixHandler);
+const generalRateLimit = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 100 * maxMultiple, // Limit each IP to 100 requests per `window` in prod, 10,000 in testing (per the winowMs time).
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const strongestRateLimit = rateLimit({
+  ...generalRateLimit,
+  limit: 10,
+});
+
+const strongRateLimit = rateLimit({
+  ...generalRateLimit,
+  limit: 100,
+});
+
+app.use((req, res, next) => {
+  const protectedPaths = ['/signup', '/login'];
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    if (
+      req.method !== 'GET' &&
+      protectedPaths.some((path) => req.path.includes(path))
+    ) {
+      return strongestRateLimit(req, res, next);
+    }
+    return strongRateLimit(req, res, next);
+  }
+  return generalRateLimit(req, res, next);
+});
+
+app.use(morgan('tiny'));
+
+app.all('*', remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
   console.log(`Express server listening at http://localhost:${port}`);
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'development') {
     broadcastDevReady(initialBuild);
   }
 });
@@ -81,7 +115,7 @@ async function reimportServer() {
   const BUILD_URL = url.pathToFileURL(BUILD_PATH).href;
 
   // use a timestamp query parameter to bust the import cache
-  return import(BUILD_URL + "?t=" + stat.mtimeMs);
+  return import(BUILD_URL + '?t=' + stat.mtimeMs);
 }
 
 /**
@@ -96,18 +130,18 @@ async function createDevRequestHandler(initialBuild) {
     // 2. tell Remix that this app server is now up-to-date and ready
     broadcastDevReady(build);
   }
-  const chokidar = await import("chokidar");
+  const chokidar = await import('chokidar');
   chokidar
     .watch(VERSION_PATH, { ignoreInitial: true })
-    .on("add", handleServerUpdate)
-    .on("change", handleServerUpdate);
+    .on('add', handleServerUpdate)
+    .on('change', handleServerUpdate);
 
   // wrap request handler to make sure its recreated with the latest build for every request
   return async (req, res, next) => {
     try {
       return createRequestHandler({
         build,
-        mode: "development",
+        mode: 'development',
       })(req, res, next);
     } catch (error) {
       next(error);
